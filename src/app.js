fdb40fc1faa5f983d0610b26a727389423eb493d
clm1010 <limin_chu@163.com>
2020-05-24 19:29:41 +0800

    调整目录结构

--- src/app.js
+++ src/app.js
@@ -28,13 +28,13 @@ app.use(
   })
 )
 
-// logger
-app.use(async (ctx, next) => {
-  const start = new Date()
-  await next()
-  const ms = new Date() - start
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
-})
+// // logger
+// app.use(async (ctx, next) => {
+//   const start = new Date()
+//   await next()
+//   const ms = new Date() - start
+//   console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+// })
 
 // routes
 app.use(index.routes(), index.allowedMethods())
