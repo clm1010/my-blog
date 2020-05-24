7442f82483530a5a5a32a0efd64fb3e69cca3edf
clm1010 <limin_chu@163.com>
2020-05-24 23:53:39 +0800

    my-blogg

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,13 +6,17 @@ router.get('/', async (ctx, next) => {
   })
 })
 
-router.get('/string', async (ctx, next) => {
-  ctx.body = 'koa2 string'
-})
-
 router.get('/json', async (ctx, next) => {
   ctx.body = {
     title: 'koa2 json'
+  }
+})
+
+router.get('/profile/:userName', async (ctx, next) => {
+  const { userName } = ctx.params
+  ctx.body = {
+    title: 'profile',
+    userName
   }
 })
 
