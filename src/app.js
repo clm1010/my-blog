a3d1c4107b4c1e099be7e6ac08eeb46d3d867ad5
clm1010 <limin_chu@163.com>
2020-05-31 23:36:04 +0800

    增加session配置

--- src/app.js
+++ src/app.js
@@ -4,6 +4,11 @@ import json from 'koa-json'
 import onerror from 'koa-onerror'
 import bodyparser from 'koa-bodyparser'
 import logger from 'koa-logger'
+import session from 'koa-generic-session'
+import redisStore from 'koa-redis'
+
+import { REDIS_CONF } from './conf/db'
+
 import index from './routes/index'
 import users from './routes/users'
 
@@ -25,6 +30,24 @@ app.use(require('koa-static')(__dirname + '/public'))
 app.use(
   views(__dirname + '/views', {
     extension: 'ejs'
+  })
+)
+
+// session 设置 加密
+app.keys = ['Clm_851010!#$']
+app.use(
+  session({
+    key: 'weibo.sid', // cookie name 默认 `koa.sid`
+    prefix: 'weibo:sess:', //redis key 的前缀，默认是`koa:sess:`
+    cookie: {
+      path: '/',
+      httpOnly: true,
+      maxAge: 24 * 60 * 60 * 1000 // cookie过期时间
+    },
+    // ttl: 24 * 60 * 60 * 1000, // redis过期时间，如果cookie配置了过期时间 这里就不需要写了
+    store: redisStore({
+      all: `${REDIS_CONF.host}:${REDIS_CONF.port}`
+    })
   })
 )
 
