6070562a8f246681670fa218098f4c1dbe3d29a0
CLM10100 <limin_chu@163.com>
2020-07-09 09:26:40 +0800

    完成 已登录状态

--- src/routes/view/user.js
+++ src/routes/view/user.js
@@ -6,17 +6,37 @@
 const router = require('koa-router')()
 
 /**
- * @description login 路由
+ * 获取登录信息
+ * @param {Object} ctx ctx
+ */
+function getLoginInfo(ctx) {
+  let data = {
+    isLogin: false // 默认未登录
+  }
+
+  const userInfo = ctx.session.userInfo
+  if (userInfo) {
+    data = {
+      isLogin: true,
+      userName: userInfo.userName
+    }
+  }
+
+  return data
+}
+
+/**
+ * login 路由
  */
 router.get('/login', async (ctx, next) => {
-  await ctx.render('login', {})
+  await ctx.render('login', getLoginInfo(ctx))
 })
 
 /**
- * @description register 路由
+ *  register 路由
  */
 router.get('/register', async (ctx, next) => {
-  await ctx.render('register', {})
+  await ctx.render('register', getLoginInfo(ctx))
 })
 
 module.exports = router
