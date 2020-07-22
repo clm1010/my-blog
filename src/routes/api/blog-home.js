086a1317c7e50af6f4334947b6b271cd40b97244
CLM10100 <limin_chu@163.com>
2020-07-22 15:23:32 +0800

    创建微博 xss 过滤和格式校验

--- src/routes/api/blog-home.js
+++ src/routes/api/blog-home.js
@@ -6,14 +6,21 @@
 const router = require('koa-router')()
 const { loginCheck } = require('../../middlewares/loginChecks')
 const { create } = require('../../controller/blog-home')
+const { genValidator } = require('../../middlewares/validator')
+const blogValidate = require('../../validator/blog')
 
 router.prefix('/api/blog')
 
 // 创建微博
-router.post('/create', loginCheck, async (ctx, next) => {
-  const { content, imgage } = ctx.request.body
-  const { id: userId } = ctx.session.userInfo
-  ctx.body = await create({ userId, content, imgage })
-})
+router.post(
+  '/create',
+  loginCheck,
+  genValidator(blogValidate),
+  async (ctx, next) => {
+    const { content, image } = ctx.request.body
+    const { id: userId } = ctx.session.userInfo
+    ctx.body = await create({ userId, content, image })
+  }
+)
 
 module.exports = router
